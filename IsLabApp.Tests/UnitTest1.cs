9dfe5a1421d84d00755f90e1c7c0643e54fcadf6
Flaves <ewero0708@gmail.com>
2026-05-06 13:35:28 +0000

    Lab10: remove GlobalUsings, explicit namespace

--- IsLabApp.Tests/UnitTest1.cs
+++ IsLabApp.Tests/UnitTest1.cs
@@ -1,12 +1,13 @@
 using Xunit;
 
-namespace IsLabApp.Tests;
-
-public class UnitTest1
+namespace IsLabApp.Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
-        Assert.Equal(1, 1);
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
     }
 }
